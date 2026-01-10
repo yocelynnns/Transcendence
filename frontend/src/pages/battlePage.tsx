e9259e144829288143154bdf670c116f58b3ce7b
Darren Fasius <darrenfasiuslim@gmail.com>
2026-01-10 15:50:11 +0800

    battle game component fix positions

--- frontend/src/pages/battlePage.tsx
+++ frontend/src/pages/battlePage.tsx
@@ -1,5 +1,15 @@
-import frontCleffa from "../assets/sprites/pokemon/normal/cleffa/front_cleffa.gif";
-import backCleffa from "../assets/sprites/pokemon/normal/togepi/back_togepi.gif";
+import React from "react";
+import cleffa from "../assets/pokemon/normal/cleffa/cleffa.gif";
+import togepi from "../assets/pokemon/normal/togepi/togepi.gif";
+import nightCaveBg from "../assets/bg/night_cave.png";
+
+import oppPlatform from "../assets/bg/pink_platform_opp.png";
+import playerPlatform from "../assets/bg/pink_platform_player.png";
+
+import oppHpBar from "../assets/health_bar/health_bar_opp.png";
+import playerHpBar from "../assets/health_bar/health_bar_player.png";
+
+import menuBar from "../assets/menu_bar/menu_bar.png"
 
 export default function Battle() {
   return (
@@ -8,61 +18,130 @@ export default function Battle() {
         position: "relative",
         width: "100vw",
         height: "100vh",
-        backgroundColor: "#0b1e5f", // navy blue night sky
+        backgroundImage: `url(${nightCaveBg})`,
+        backgroundSize: "cover",
+        backgroundPosition: "center",
+        backgroundRepeat: "no-repeat",
         overflow: "hidden",
       }}
     >
-      {/* Enemy Pokémon */}
+      {/* enemy container */}
       <div
         style={{
           position: "absolute",
-          top: "10%",
-          right: "20%",
-          textAlign: "center",
+          left: "31.25%",
+          top: "16.67%",
+          width: "62.5%",
+          height: "38.89%",
+          // backgroundColor: "#999",
         }}
       >
+        {/* enemy platform */}
         <img
-          src={frontCleffa}
-          alt="Front Cleffa"
-          style={{ width: "300px", display: "block" }}
+          src={oppPlatform}
+          alt="Enemy platform"
+          style={{
+            position: "absolute",
+            left: "40%",
+            top: "45%",
+            width: "60%",
+            imageRendering: "pixelated",
+          }}
+        />
+        {/* enemy pokemon */}
+        <img
+          src={cleffa}
+          alt="Enemy Pokemon"
+          style={{
+            position: "absolute",
+            left: "55%",
+            top: "5%",
+            width: "30%",
+            imageRendering: "pixelated",
+          }}
+        />
+        {/* enemy hp bar */}
+        <img
+          src={oppHpBar}
+          alt="Enemy Hp Bar"
+          style={{
+            position: "absolute",
+            left: "-10%",
+            top: "-5%",
+            width: "55%",
+            imageRendering: "pixelated",
+          }}
         />
       </div>
 
-      {/* Your Pokémon */}
+      {/* player container */}
       <div
         style={{
           position: "absolute",
-          bottom: "150px", // leave space for menu bar
-          left: "20%",
-          textAlign: "center",
+          top: "50%",
+          width: "75%",
+          height: "33%",
+          // backgroundColor: "#999",
         }}
       >
+        {/* player platform */}
         <img
-          src={backCleffa}
-          alt="Back Cleffa"
-          style={{ width: "400px", display: "block" }}
+          src={playerPlatform}
+          alt="Player platform"
+          style={{
+            position: "absolute",
+            bottom: 0,
+            width: "83.33%",
+            imageRendering: "pixelated",
+          }}
+        />
+        {/* player pokemon */}
+        <img
+          src={togepi}
+          alt="Player Pokemon"
+          style={{
+            position: "absolute",
+            left: "16.67%",
+            top: "-30%",
+            width: "45%",
+            imageRendering: "pixelated",
+          }}
+        />
+        {/* player hp bar */}
+        <img
+          src={playerHpBar}
+          alt="Player Hp Bar"
+          style={{
+            position: "absolute",
+            top: "10%",
+            left: "65%",
+            width: "45.83%",
+            imageRendering: "pixelated",
+          }}
         />
       </div>
-
-      {/* Menu bar */}
+      
+      {/* menu bar container */}
       <div
         style={{
           position: "absolute",
-          bottom: 0,
+          top: "83.33%",
           width: "100%",
-          height: "150px",
-          backgroundColor: "#999",
-          display: "flex",
-          justifyContent: "space-around",
-          alignItems: "center",
-          padding: "0 2rem",
-          boxSizing: "border-box",
+          height: "16.67%",
+          // backgroundImage: `url(${menuBar})`,
         }}
       >
-        <button>poke1</button>
-        <button>poke2</button>
-        <button>attack</button>
-        <button>surrender</button>
+        <img
+          src={menuBar}
+          alt="menu Bar"
+          style={{
+            position: "absolute",
+            left: "0%",
+            width: "100%",
+            height: "100%",
+            imageRendering: "pixelated",
+          }}
+        />
       </div>
     </div>
   );
