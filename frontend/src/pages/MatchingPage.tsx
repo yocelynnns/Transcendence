9f35eab0beeb686c11ee4738e2c1a766b4d7cb54
yocelynnns <ytheonas05@gmail.com>
2026-02-11 23:18:05 +0800

    battle page, team select

--- frontend/src/pages/MatchingPage.tsx
+++ frontend/src/pages/MatchingPage.tsx
@@ -1,3 +1,4 @@
+// src/pages/matchingpage.tsx
 import { useEffect, useState, useMemo, Dispatch, useRef } from "react";
 import { useNavigate } from "react-router-dom";
 import type { AvatarData } from "../types/avatarTypes";
